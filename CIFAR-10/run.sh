2c154a5a4c664a4317d61981c8255d81c0aec309
nblt <ltwtzjh@126.com>
2021-03-20 09:47:06 +0800

    Update run.sh

--- CIFAR-10/run.sh
+++ CIFAR-10/run.sh
@@ -1,11 +1,12 @@
 
 #!/bin/bash
+# total time comparison on one GPU
 
 for model in resnet20
 do
 
-    python -u train_sgd.py --randomseed 1 --arch=$model --epochs=201  --save-dir=save_$model |& tee -a log_$model
-
-    python -u train_pbfgs_cuda.py --epochs 20 --alpha 0.0 --params_end 51  --batch-size 1024  --n_components 40 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+    CUDA_VISIBLE_DEVICES=0 python -u train_sgd.py --randomseed 1 --arch=$model --epochs=201  --save-dir=save_$model |& tee -a log_$model
+    
+    CUDA_VISIBLE_DEVICES=0 python -u train_pbfgs_cuda.py --epochs 20 --alpha 0.01 --params_end 81  --batch-size 1024  --n_components 40 --resume=./save_resnet20/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
 
 done
