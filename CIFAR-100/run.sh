79bd1e7973af517c23dd2360a0f24214c39d2de8
nblt <ltwtzjh@126.com>
2021-03-20 09:40:48 +0800

    Update run.sh

--- CIFAR-100/run.sh
+++ CIFAR-100/run.sh
@@ -5,6 +5,6 @@ do
 
     python -u train_sgd.py --randomseed 1 --arch=$model --epochs=201  --save-dir=save_$model |& tee -a log_$model
 
-    python -u train_v9_mutigpu.py --epochs 20 --alpha 0.0 --params_start 0 --params_end 101  --batch-size 1024  --n_components 40 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+    python -u train_pbfgs.py --epochs 20 --alpha 0.0 --params_start 0 --params_end 101  --batch-size 1024  --n_components 40 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
 
 done
