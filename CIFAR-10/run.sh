7eb8c577c3df34b7fcb304350e3d3894299fc332
nblt <ltwtzjh@126.com>
2021-03-20 09:42:47 +0800

    Update run.sh

--- CIFAR-10/run.sh
+++ CIFAR-10/run.sh
@@ -6,10 +6,6 @@ do
 
     python -u train_sgd.py --randomseed 1 --arch=$model --epochs=201  --save-dir=save_$model |& tee -a log_$model
 
-    python -u train_v9_mutigpu.py --epochs 20 --alpha 0.0 --params_end 51  --batch-size 1024  --n_components 40 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
- 
-	# python -u train_v9_acc_grad.py --epochs 20 --alpha 0.0 --params_end 51  --batch-size 128  --n_components 40 --resume=./save_$model/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
-    # python -u train_v9_acc_grad.py --epochs 20 --alpha 0.01 --params_end 51  --batch-size 128  --n_components 40 --resume=./save_$model/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
-    # python -u train_v9_acc_grad.py --epochs 20 --alpha 0.0 --params_end 101  --batch-size 128  --n_components 40 --resume=./save_$model/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
-    # python -u train_v9_acc_grad.py --epochs 20 --alpha 0.01 --params_end 101  --batch-size 128  --n_components 40 --resume=./save_$model/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+    python -u train_pbfgs_cuda.py --epochs 20 --alpha 0.0 --params_end 51  --batch-size 1024  --n_components 40 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+
 done
