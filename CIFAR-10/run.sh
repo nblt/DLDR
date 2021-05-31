7fc577d28c1ee2fe66009f987ae7d5777410defb
nblt <ltwtzjh@126.com>
2021-05-31 16:37:18 +0800

    Update run.sh

--- CIFAR-10/run.sh
+++ CIFAR-10/run.sh
@@ -4,7 +4,7 @@
 for model in resnet20
 do
 
-    CUDA_VISIBLE_DEVICES=1 python -u train_sgd.py --randomseed 1 --arch=$model --epochs=50  --save-dir=save_$model |& tee -a log_$model
+    CUDA_VISIBLE_DEVICES=1 python -u train_sgd.py --randomseed 0 --arch=$model --epochs=50  --save-dir=save_$model |& tee -a log_$model
     
     CUDA_VISIBLE_DEVICES=0 python -u train_pbfgs_cuda.py --epochs 20 --alpha 0.0 --params_end 51  --batch-size 1024  --n_components 40 --resume=./save_resnet20/checkpoint_refine_0.th --arch=$model  --save-dir=save_$model |& tee -a log_$model 
 
