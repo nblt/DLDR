64c4748137b1501c15ece73ee3b60da66c6ee1d1
nblt <ltwtzjh@126.com>
2021-08-24 13:21:58 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -296,32 +296,6 @@ def get_datasets(args):
     
     return train_loader, val_loader
 
-
-class LeNet5(nn.Module):
-    def __init__(self):
-        # nn.Module子类的函数必须在构造函数中执行父类的构造函数
-        # 下式等价于nn.Module.__init__(self)
-        super(LeNet5, self).__init__()
-        # 卷积层"3"表示输入图片为单通道，"6"表示输出通道数，"5"表示卷积核为5*5
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        # 卷积层
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # 仿射层/全连接层，y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        # 卷积 -> 激活 -> 池化
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        # reshape，"-1"表示自适应
-        x = x.view(x.size()[0], -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
 def get_model(args):
     if args.datasets == 'ImageNet':
         return models_imagenet.__dict__[args.arch]()
@@ -471,8 +445,4 @@ def get_model(args):
             net = resnet.__dict__[args.arch](num_classes=num_class)
 
         return net
-
-    if args.arch == 'LeNet5':
-        return LeNet5()
-    else:
-        return resnet.__dict__[args.arch](num_classes=num_class)
+    return resnet.__dict__[args.arch](num_classes=num_class)
