fd30522cab12a96263684d7fbc691d90ce4b5527
Your Name <you@example.com>
2022-01-11 12:16:23 +0800

    Add:update run shell

--- run/run_psgd_dp.sh
+++ run/run_psgd_dp.sh
@@ -1,18 +1,17 @@
-c=0
-datasets=CIFAR100
-model=resnet101
-pretrain='output/CIFAR100/sgd/sgd-CIFAR100-resnet101-128-0.1-20220104-171853'
-CUDA_VISIBLE_DEVICES=2,3 python -u train_psgd_dp.py \
+datasets=CIFAR10
+model=resnet20
+pretrain='output/CIFAR10/sgd_ddp_test_sample/sgd_ddp-CIFAR10-resnet20-128-0.1-20220110-141224'
+CUDA_VISIBLE_DEVICES=2 python -u train_psgd_dp.py \
     --epochs=40 \
     --arch=$model \
     --datasets=$datasets \
     --batch_size=128 \
     --lr=1 \
-    --corrupt=$c \
+    --sample_mode=epoch \
     --params_start=0 \
     --params_end=80 \
     --n_components=40 \
     --pretrain_dir=$pretrain \
-    --save_dir=output/${datasets}/psgd_dp \
+    --save_dir=output/${datasets}/psgd_dp_test \
     # --log_wandb \
     # --project=DLDR
