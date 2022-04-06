7381bbee79920058d3f70ec787fcaf61341ee28d
nblt <ltwtzjh@126.com>
2022-04-06 09:39:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,9 +21,11 @@ c=0
 datasets=ImageNet
 for model in resnet18
 do
-	CUDA_VISIBLE_DEVICES=0,1,2,3  python3 main.py -a $model --epochs 90  --dist-url 'tcp://127.0.0.1:8888' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 /home/datasets/ILSVRC2012
 
-	CUDA_VISIBLE_DEVICES=0,1,2 python -u train_pbfgs_imagenet.py --epochs 4 --print-freq 1000 --datasets $datasets --corrupt $c --alpha 0 --params_start 0 --params_end 241  --batch-size 256  --n_components 120 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+    CUDA_VISIBLE_DEVICES=0,1,2,3  python3 main.py -a $model --epochs 90  --dist-url 'tcp://127.0.0.1:8888' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 /home/datasets/ILSVRC2012
+    
+    CUDA_VISIBLE_DEVICES=0,1,2 python -u train_pbfgs_imagenet.py --epochs 4 --print-freq 1000 --datasets $datasets --corrupt $c --alpha 0 --params_start 0 --params_end 241  --batch-size 256  --n_components 120 --arch=$model  --save-dir=save_$model |& tee -a log_$model 
+
 done
 
 ############################ fine-tune experiments ##########################
